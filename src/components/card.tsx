3d8e66968a3615f71a0f5bfd8267e08f1975ca1b
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2024-02-10 13:23:34 +0100

    Finished skills styling

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,7 +15,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ( { onClick, properties } ) => {
 
-    const [focusedSkill, setFocusedSkill] = useState<number | null>(null);
+    const [focusedSkill, setFocusedSkill] = useState<number>(0);
     const [firstSkillClassName, setFirstSkillClassName] = useState<string>('first-skill');
     const [secondSkillClassName, setSecondSkillClassName] = useState<string>('second-skill');
     const [thirdSkillClassName, setThirdSkillClassName] = useState<string>('third-skill');
@@ -28,62 +28,124 @@ const Card: React.FC<CardProps> = ( { onClick, properties } ) => {
 
 
 
-    const handleFocus = (index: number) => {
+    const handleFocus = (skill: number) => {
 
-        if (index === 0) {
+        if (skill === 1) {
             setFirstSkillClassName('first-skill-focused');
             setSecondSkillClassName('second-skill-hidden');
             setThirdSkillClassName('third-skill-hidden');
         }
 
-        else if (index === 1) {
+        else if (skill === 2) {
             setSecondSkillClassName('second-skill-focused');
+            setFirstSkillClassName('first-skill-hidden');
+            setThirdSkillClassName('third-skill-hidden');
         }
 
-        else if (index === 2) {
+        else if (skill === 3) {
             setThirdSkillClassName('third-skill-focused');
+            setFirstSkillClassName('first-skill-hidden');
+            setSecondSkillClassName('second-skill-hidden');
+        }
+
+        else if(skill === 0){
+            setFirstSkillClassName('first-skill');
+            setSecondSkillClassName('second-skill');
+            setThirdSkillClassName('third-skill');
         }
     }
 
     return(
         <div className='card' onClick={onClick}>
             <div className='card-back'>
-                <h1 className='back-subtitle'>Technologies utilisées</h1>
-                <div className='logos-container'>
-                    {properties.tehcnologiesIcon.map((icon, index) => {
-                        return <img key={index} src={icon} className="tech-logo"/>
-                    })}
-                </div>
-                <div className='tech-container'>
-                    {properties.technologies.map((tech, index) => {
-                        return <h2 key={index} className='tech-text'>{tech}</h2>
-                    })}
-                </div>
-                <h1 className='back-subtitle'>Compétences du PN</h1>
-                <div className='skills-container'>
-                    {
-                        properties.skills.map((skill, index) => {
-                            if (skill === 1) {
-                                return <div className={firstSkillClassName} onMouseEnter={
-                                    () => { 
-                                        setFocusedSkill(index)
-                                        handleFocus(index)
-                                    }
-                                }                                             
-                                onMouseLeave={() => setFocusedSkill(null)}> <p className='skill-number'>1</p> </div>
-                            }
-                            if (skill === 2) {
-                                return <div className={secondSkillClassName} onMouseEnter={() => setFocusedSkill(index)} onMouseLeave={() => setFocusedSkill(null)}> <p className='skill-number'>2</p> </div>
-                            }
-                            if (skill === 3) {
-                                return <div className={thirdSkillClassName} onMouseEnter={() => setFocusedSkill(index)} onMouseLeave={() => setFocusedSkill(null)}> <p className='skill-number'>3</p> </div>
-                            }
-                        })
-                    }
 
-                    {/* properties.skills.map((skill, index) => {
-                        return <h2 key={index} className='skill-text'>{skill}</h2>
-                    }) */}
+                <div className='back-top'>
+                    <h1 className='back-subtitle'>Technologies utilisées</h1>
+                    <div className='logos-container'>
+                        {properties.tehcnologiesIcon.map((icon, index) => {
+                            return <img key={index} src={icon} className="tech-logo"/>
+                        })}
+                    </div>
+                    <div className='tech-container'>
+                        {properties.technologies.map((tech, index) => {
+                            return <h2 key={index} className='tech-text'>{tech}</h2>
+                        })}
+                    </div>
+                    </div>
+
+                <div className='back-bottom'>
+                    <h1 className='back-subtitle'>Compétences du PN</h1>
+                    <div className='skills-container'>
+                        {
+                            properties.skills.map((skill) => {
+
+                                if (skill === 1) {
+                                    if(focusedSkill === 1){
+                                        return (<div className='first-skill-focused' onMouseLeave={() => {
+                                            setFocusedSkill(0);
+                                            handleFocus(0)}}>
+                                            <p className='skill-number-focused'>1</p>
+                                            <h2 className='skill-text'>{skills[0]}</h2>
+                                        </div>)
+                                    }
+
+                                    return (
+                                    
+                                    <div className={firstSkillClassName} 
+                                    onMouseEnter={() => {
+                                            setFocusedSkill(skill);
+                                            handleFocus(skill);
+                                        }}>
+                                    <p className='skill-number'>1</p> </div> 
+                                    )}
+
+
+                                else if (skill === 2) {
+
+                                    if(focusedSkill === 2){
+                                        return (<div className='second-skill-focused' onMouseLeave={() => {
+                                            setFocusedSkill(0);
+                                            handleFocus(0)}}>
+                                            <p className='skill-number-focused'>2</p>
+                                            <h2 className='skill-text'>{skills[1]}</h2>
+                                        </div>)
+                                    }
+                                    
+                                    return <div className={secondSkillClassName} onMouseEnter={
+                                        () => {
+                                            setFocusedSkill(skill);
+                                            handleFocus(skill);
+                                        }
+                                    } 
+                                    onMouseLeave={() => {
+                                        setFocusedSkill(0);
+                                        handleFocus(0);
+                                    }}> <p className='skill-number'>2</p> </div>
+                                }
+
+                                else {
+
+                                    if(focusedSkill === 3){
+                                        return (<div className='third-skill-focused' onMouseLeave={() => {
+                                            setFocusedSkill(0);
+                                            handleFocus(0)}}>
+                                            <p className='skill-number-focused'>3</p>
+                                            <h2 className='skill-text'>{skills[2]}</h2>
+                                        </div>)
+                                    }
+
+                                    return <div className={thirdSkillClassName} onMouseEnter={() => {
+                                        setFocusedSkill(skill);
+                                        handleFocus(skill)
+                                    }} 
+                                    onMouseLeave={() => {
+                                        setFocusedSkill(0);
+                                        handleFocus(0);
+                                    }}> <p className='skill-number'>3</p> </div>
+                                }
+                            })
+                        }
+                    </div>
                 </div>
             </div>
 
