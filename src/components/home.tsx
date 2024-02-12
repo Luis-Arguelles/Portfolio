fca98c6031db2fe4f5d39c94e7518cec50b063ae
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2024-02-12 16:45:39 +0100

    Finished adding technologies'icons

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -12,12 +12,16 @@ import Laravel from "../assets/techIcons/Laravel.png"
 import SpringBoot from "../assets/techIcons/SpringBoot.png"
 import SQL from "../assets/techIcons/MySQL.png"
 import MongoDB from "../assets/techIcons/MongoDB.png"
-import Java from "../assets/techIcons/icons8-java-240.png"
+import Java from "../assets/techIcons/Java.png"
 import Csharp from "../assets/techIcons/Csharp.png"
 import CPlusPlus from "../assets/techIcons/C++.png"
-import Express from "../assets/techIcons/icons8-express-js.svg"
+import Express from "../assets/techIcons/Express.svg"
 import Git from "../assets/techIcons/Git.png"
-
+import Mongoose from "../assets/techIcons/Mongoose.png"
+import TypeScript from "../assets/techIcons/TypeScript.png"
+import Axios from "../assets/techIcons/Axios.png"
+import Redux from "../assets/techIcons/Redux.svg"
+import ReactNavigation from "../assets/techIcons/ReactNavigation.svg"
 
 const Home = () => {
 
@@ -34,23 +38,31 @@ const Home = () => {
                 <h2>Technologies</h2>
                 <div className="technologies-container">
                     <TechComponent techName="JavaScript" techLogo={JavaScript}/>
+                    <TechComponent techName="TypeScript" techLogo={TypeScript}/>
                     <TechComponent techName="HTML" techLogo={HTML}/>
                     <TechComponent techName="CSS" techLogo={CSS}/>
+                    <TechComponent techName="Express" techLogo={Express}/>
+
+                </div>
+                <div className="technologies-container">
                     <TechComponent techName="React" techLogo={ReactLogo}/>
                     <TechComponent techName="React Native" techLogo={ReactNative}/>
+                    <TechComponent techName="React Navigation" techLogo={ReactNavigation}/>
+                    <TechComponent techName="Redux" techLogo={Redux}/>
+                    <TechComponent techName="Axios" techLogo={Axios}/>
+                </div>
+                <div className="technologies-container">
+                    <TechComponent techName="Laravel" techLogo={Laravel}/>
+                    <TechComponent techName="SpringBoot" techLogo={SpringBoot}/>
+                    <TechComponent techName="MongoDB" techLogo={MongoDB}/>
+                    <TechComponent techName="Mongoose" techLogo={Mongoose}/>
+                    <TechComponent techName="SQL" techLogo={SQL}/>
                 </div>
                 <div className="technologies-container">
                     <TechComponent techName="Python" techLogo={Python}/>
-                    <TechComponent techName="Laravel" techLogo={Laravel}/>
-                    <TechComponent techName="SpringBoot" techLogo={SpringBoot}/>
-                    <TechComponent techName="SQL" techLogo={SQL}/>
-                    <TechComponent techName="MongoDB" techLogo={MongoDB}/>
-                </div>
-                <div className="technologies-container">
                     <TechComponent techName="Java" techLogo={Java}/>
                     <TechComponent techName="C#" techLogo={Csharp}/>
                     <TechComponent techName="C++" techLogo={CPlusPlus}/>
-                    <TechComponent techName="Express" techLogo={Express}/>
                     <TechComponent techName="Git" techLogo={Git}/>
                 </div>
             </div>
